8cd5e787b3e6923d585ef3feaade472e109091cf
Matthew Schuelke <matt@themadstatter.com>
2025-10-21 12:58:28 -0500

    rm space also

--- stackoverflow/remove_citeproc_footnote_citation_parentheses/citations_in_footnotes_fixer.lua
+++ stackoverflow/remove_citeproc_footnote_citation_parentheses/citations_in_footnotes_fixer.lua
@@ -1,8 +1,11 @@
 function Note(note)
   return pandoc.walk_inline(note, {Cite = function(cit)
-      if (cit.citations[1].mode=='NormalCitation') and (#cit.content > 1) then 
-        if cit.content[2].text=='(' then
-          cit.content[2].text='';
+      if (cit.citations[1].mode=='NormalCitation') and (#cit.content > 1) then
+        if cit.content[1].t == 'Space' then
+          table.remove(cit.content, 1) -- remove space and shift everything left
+        end
+        if cit.content[1].t == 'Str' and cit.content[1].text=='(' then
+          cit.content[1].text='';
         end
         if cit.content[#cit.content].text==')' then
           cit.content[#cit.content].text=''
